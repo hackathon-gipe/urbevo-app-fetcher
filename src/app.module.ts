9d193d2d4588fb3544f0f545f6870846a505bca3
Ale Sanchez <alesanchezmedina@gmail.com>
2023-05-08 22:12:30 +0200

    feat: ✨ added root endpoint for healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { NeedsModule } from './needs/needs.module';
 import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [NeedsModule, LoggerModule.forRoot()],
-  controllers: [],
+  imports: [NeedsModule, LoggerModule.forRoot(), ConfigModule.forRoot()],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
